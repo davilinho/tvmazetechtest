0c2e8c09d0e07b3e965a535b2c6c6fe5ae35be25
David Martin <david.martin@mediquo.com>
2021-11-16 10:55:20 +0100

    Add pagination

--- tvmazetechtest/Source/Features/List/Presentation/ListViewModel.swift
+++ tvmazetechtest/Source/Features/List/Presentation/ListViewModel.swift
@@ -11,30 +11,50 @@ class ListViewModel: InjectableComponent & BaseViewModel {
     @Inject
     private var useCase: ListUseCase
 
+    private var currentPage: Int = 0
+
+    var isLoading: Bool = true
     var models = Observable<[Show]>()
 
     func onViewDidAppear() {
+        self.resetPage()
         self.useCase.fetch() { [weak self] in
             guard let self = self else { return }
             self.update(models: $0)
         }
     }
 
-    func fecthNext(page: Int) {
-        self.useCase.fetch(by: page) { [weak self] in
+    func fecthNext() {
+        self.setIsLoading()
+        self.increasePage()
+        self.useCase.fetch(by: self.currentPage) { [weak self] in
             guard let self = self else { return }
             self.update(models: $0)
         }
     }
 
     func search(by query: String? = nil) {
+        self.resetPage()
         self.useCase.search(by: query) { [weak self] in
             guard let self = self else { return }
             self.update(models: $0.compactMap { $0.show })
         }
     }
 
+    private func setIsLoading() {
+        self.isLoading = true
+    }
+
+    private func resetPage() {
+        self.currentPage = 0
+    }
+
+    private func increasePage() {
+        self.currentPage += 1
+    }
+
     private func update(models: [Show]) {
+        self.isLoading = false
         self.models.value = models
     }
 }
